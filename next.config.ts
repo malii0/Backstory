257095e9b26d9609bdaefc977547cf8eea497481
Muhammed Ali Onurlucan <m.alionurlucan@gmail.com>
2026-08-31 02:34:51 +0300

    chore: disable next.js image optimization

--- next.config.ts
+++ next.config.ts
@@ -39,11 +39,7 @@ const nextConfig = {
     ];
   },
   images: {
-    minimumCacheTTL: 2678400,
-    formats: ["image/webp"],
-    qualities: [75],
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    unoptimized: true, // Vercel'in resimleri dönüştürmesini (transformations) tamamen kapatır
     remotePatterns: [
       {
         protocol: "https",
